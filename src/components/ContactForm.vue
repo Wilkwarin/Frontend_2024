ccd36f1359535e1ca4a16b7457b5195680d91968
Wilkwarin <wilkwarin@gmail.com>
2025-01-14 20:20:42 +0100

    Test

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -35,10 +35,7 @@ export default {
       if (this.name && this.email) {
         const submission = { name: this.name, email: this.email };
         contactStore.addContactSubmission(submission);
-        console.log("Submission object:", submission);
         this.message = `Thank you, ${this.name}! Your message has been submitted.`;
-        console.log("Contact store:", contactStore);
-        console.log("Before submission:", this.name, this.email);
         this.name = "";
         this.email = "";
       } else {
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <h1>Blog</h1>
+
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="title">Заголовок:</label>
+        <input
+          id="title"
+          v-model="newPost.title"
+          type="text"
+          placeholder="Введите заголовок"
+          required
+        />
+      </div>
+      <div>
+        <label for="content">Содержание:</label>
+        <textarea
+          id="content"
+          v-model="newPost.content"
+          placeholder="Введите содержание"
+          required
+        ></textarea>
+      </div>
+      <button type="submit">Добавить статью</button>
+    </form>
+
     <div class="post-list">
-      <PostCard 
-        v-for="post in posts" 
-        :key="post.id" 
-        :title="post.title" 
-        :content="post.content" 
+      <PostCard
+        v-for="post in posts"
+        :key="post.id"
+        :title="post.title"
+        :content="post.content"
       />
     </div>
   </div>
@@ -21,12 +45,32 @@ export default {
   components: { PostCard },
   data() {
     return {
-      postStore: usePostStore(),
+      newPost: {
+        title: '',
+        content: '',
+      },
     };
   },
   computed: {
     posts() {
       return this.postStore.posts;
+    },
+    postStore() {
+      return usePostStore();
+    },
+  },
+  methods: {
+    handleSubmit() {
+      if (this.newPost.title && this.newPost.content) {
+        const post = {
+          id: Date.now(),
+          title: this.newPost.title,
+          content: this.newPost.content,
+        };
+        this.postStore.addPost(post);
+        this.newPost.title = '';
+        this.newPost.content = '';
+      }
     },
   },
 };
